d0cf355cf9c24fbe079d90bf38117e37459ef25b
Ricky Shin <rich@rshin.dev>
2021-08-09 19:14:49 -0400

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div style={{textAlign: 'center', paddingBottom: '1%' }}>
-             <a href="https://www.rshin.dev/" style={{textDecoration: 'none', fontStyle: 'italic'}} target="_blank" rel="noreferrer">by Richard Shin</a>
+             <a href="https://www.rshin.dev/" style={{textDecoration: 'none', fontStyle: 'italic'}} target="_blank" rel="noreferrer">by rs4078</a>
         </div>
     )
 }
